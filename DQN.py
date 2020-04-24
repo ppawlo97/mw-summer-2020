67e159a431897a3de048a2054f59a09a64ec0201
Piotr Pawłowski <ppawlo97@gmail.com>
2020-04-24 18:56:41 +0200

    dqn and replay refactor;

--- DQN.py
+++ DQN.py
@@ -1,77 +1,169 @@
 import random
+from typing import Any
+from typing import List
+from typing import Tuple
+from typing import Union
 
 import tensorflow as tf
+from tensorflow.keras.losses import mean_squared_error
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import MeanSquaredError
 
 from MLP import MLP
 from ReplayBuffer import ReplayBuffer 
 
-# TODO: add type hints + docstrings
+
 class DQN:
     def __init__(self,
-                 num_actions,
-                 learning_rate = 0.05,
-                 discount_factor = 0.99,
-                 epsilon = 1,
-                 epsilon_dec = 0.99,
-                 epsilon_end = 0.01,
-                 memory_size = 1_000_000):
+                 num_actions: int,
+                 mlp_hidden_units: Tuple[int] = (256, 256),
+                 learning_rate: float = 0.00005,
+                 discount_factor: float = 0.95,
+                 epsilon: float = 0.8,
+                 eps_decrement: float = 0.99,
+                 eps_minimum: float = 0.01,
+                 memory_size: int = 20_000,
+                 min_memory: int = 3000):
+        """
+        Deep Q Learning Agent
+        with Multilayer Perceptron Q Network.
+
+        Parameters
+        ----------
+        num_actions: int
+            Number of actions that the agent
+            can take in the environment.
+        mlp_hidden_units: Tuple[int]
+            Number of units for every hidden layer
+            of the MLP Q Network.
+        learning_rate: float
+            Optimization step size.
+        discount_factor: float
+            Factor that determines how much agent
+            takes future rewards into account.
+        epsilon: float
+            Initial probability of taking a random action.
+            A.k.a. `epsilon` from epsilon-greedy exploration strategy
+        eps_decrement: float
+            Decrement factor of epsilon parameter.
+        eps_minimum: float
+            Minimum epsilon.
+            Epsilon will not be reduced below this value.
+        memory_size: int
+            Maximum number of past steps stored in the ReplayBuffer.
+        min_memory: int
+            Minimum number of past steps in the ReplayBuffer
+            before agent starts learning.
+
+        """
+        self._memory = ReplayBuffer(memory_size)
+        self._net = MLP(num_actions, mlp_hidden_units)
+        self._optimizer = Adam(learning_rate)
+
+        self._min_memory = min_memory
+        self._discount_factor = discount_factor
+        self._action_space = list(range(num_actions))
+        self._eps = {"epsilon": epsilon,
+                     "eps_decrement": eps_decrement,
+                     "eps_minimum": eps_minimum}
+
+
+    def extend_memory(self,
+                      state: List[Any],
+                      action: int,
+                      reward: Union[float, int],
+                      next_state: List[Any],
+                      done: bool):
+        """
+        Extends agent's memory with additional
+        environment step.
         
-        self.memory = ReplayBuffer(memory_size)
-        self.net = MLP(num_actions)
-        self.net.compile(optimizer=Adam(learning_rate), loss=MeanSquaredError())
+        Parameters
+        ----------
+        state: List[Any]
+            Current state of the environment.
+        action: int
+            Integer representation of an action
+            taken by the agent in the current state.
+        reward: Union[float, int]
+            Reward for the action taken in current state.
+        next_state: List[Any]
+            State of the environment after action was taken.
+        done: bool
+            Whether the episode is over.
+        
+        """
+        self._memory.store_step(state=state,
+                                action=action,
+                                reward=reward, 
+                                next_state=next_state,
+                                done= done)
+
     
-        self.discount_factor = discount_factor
-        self.action_space = list(range(num_actions))
-        self.eps = {"epsilon": epsilon,
-                    "epsilon_dec": epsilon_dec,
-                    "epsilon_end": epsilon_end}
+    def predict(self, state: List[Union[float, int]]) -> int:
+        """
+        Determines which action should be taken
+        given current state of the environment
+        
+        Notes
+        -----
+        Sometimes, the agent takes random action with
+        probability epsilon.
 
+        Parameters
+        ----------
+        state: List[Union[float, int]]
+            Current state of the environment represented
+            in numerical form.
 
-    def extend_memory(self, state, action, reward, next_state, done):
-        self.memory.store_step(state, action, reward, next_state, done)
-
-    
-    def predict(self, state):
-        if random.random() < self.eps["epsilon"]:
-            action = random.choice(self.action_space)
+        Returns
+        -------
+        int
+            Selected action
+        
+        """
+        if random.random() < self._eps["epsilon"]:
+            action = random.choice(self._action_space)
         else:
             state = tf.expand_dims(state, axis=0)
-            action = tf.argmax(self.net(state),
+            action = tf.argmax(self._net(state),
                                axis=-1)[0].numpy()
         
         return action
 
 
-    def fit(self, batch_size):
-        if batch_size > len(self.memory):
+    def fit(self, batch_size: int):
+        """
+        Trains the agent.
+
+        Parameters
+        ----------
+        batch_size: int
+            Number of memories to fit the Q Network on.
+        
+        """
+        if max(self._min_memory, batch_size) > len(self._memory):
             return
 
-        batch = self.memory.sample(batch_size)
+        batch = self._memory.sample(batch_size)
 
         states = tf.stack(batch["state"], axis=0)
         next_states = tf.stack(batch["next_state"], axis=0)
         actions = tf.one_hot(indices=batch["action"],
-                             depth=len(self.action_space))
-        actions = tf.cast(actions, dtype=tf.bool)
-
-        q_values = self.net(states)
-        q_values_next = tf.reduce_max(self.net(next_states), axis=-1)
-
-        discounted_rewards = (tf.convert_to_tensor(batch["reward"]) +
-                              self.discount_factor * 
-                              q_values_next * 
-                              (1 - tf.convert_to_tensor(batch["done"])))
-        discounted_rewards = tf.repeat(tf.expand_dims(discounted_rewards, -1),
-                                       repeats=2,
-                                       axis=-1)
+                             depth=len(self._action_space))
         
-        q_target = tf.where(actions,
-                            discounted_rewards,
-                            q_values)
+        q_values_next = tf.reduce_max(self._net(next_states), axis=-1)
+        q_target = (tf.convert_to_tensor(batch["reward"]) +
+                    self._discount_factor * 
+                    q_values_next * 
+                    (1 - tf.convert_to_tensor(batch["done"])))
         
-        self.net.fit(states, q_target, verbose=0)
+        with tf.GradientTape() as tape:
+            q_values = self._net(states)
+            prediction = tf.reduce_sum(q_values * actions, axis=1)
+            loss = mean_squared_error(q_target, prediction)
+        weights = self._net.trainable_variables
+        gradients = tape.gradient(loss, weights)
+        self._optimizer.apply_gradients(zip(gradients, weights))
 
-        if self.eps["epsilon_end"] > self.eps["epsilon"]:
-            self.eps["epsilon"] *= self.eps["epsilon_dec"]
+        if self._eps["eps_minimum"] < self._eps["epsilon"]:
+            self._eps["epsilon"] *= self._eps["eps_decrement"]
--- ReplayBuffer.py
+++ ReplayBuffer.py
@@ -1,9 +1,22 @@
 import random
 from collections import deque
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Union
 
-# TODO: add docstring + type hints
+
 class ReplayBuffer:
     def __init__(self, max_size: int):
+        """
+        Agent's memory.
+
+        Parameters
+        ----------
+        max_size: int
+            Maximum memory size.
+
+        """
         self.memory = deque(maxlen=max_size)
 
 
@@ -11,7 +24,30 @@ class ReplayBuffer:
         return len(self.memory)
 
 
-    def store_step(self, state, action, reward, next_state, done):
+    def store_step(self,
+                   state: List[Any],
+                   action: int,
+                   reward: Union[float, int],
+                   next_state: List[Any],
+                   done: bool):
+        """
+        Stores environment's step in memory.
+        
+        Parameters
+        ----------
+        state: List[Any]
+            Current state of the environment.
+        action: int
+            Integer representation of an action
+            taken by the agent in the current state.
+        reward: Union[float, int]
+            Reward for the action taken in current state.
+        next_state: List[Any]
+            State of the environment after action was taken.
+        done: bool
+            Whether the episode is over.
+        
+        """
         step = dict(state=state,
                     action=action,
                     reward=reward,
@@ -20,7 +56,20 @@ class ReplayBuffer:
         self.memory.append(step)
 
     
-    def sample(self, batch_size: int):
+    def sample(self, batch_size: int) -> Dict:
+        """
+        Samples `batch_size` historical steps from the memory.
+
+        Parameters
+        ----------
+        batch_size: int
+            Number of steps to sample.
+        
+        Returns
+        -------
+        Dict
+        
+        """
         batch = random.sample(self.memory, batch_size)
         batch = {key: [obs[key] for obs in batch]
                     for key in batch[0]}
